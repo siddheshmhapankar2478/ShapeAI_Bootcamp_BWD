0e2faf91d43fefdbc248f94aaee6f7f991a78b87
SiddheshMhapankar <72028899+SiddheshMhapankar@users.noreply.github.com>
2021-06-27 12:01:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Info from "./Info";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+    </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div>
+    <div class="note">
+      <h1>Python</h1>
+      <p>Python is an interpreted high-level general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation.</p>
+    </div>
+    <div class="note">
+      <h1>Java</h1>
+      <p>Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.</p>
+    </div>
+    <div class="note">
+      <h1>JavaScript</h1>
+      <p>JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.</p>
+    </div>
+    <div class="note">
+      <h1>C++</h1>
+      <p>C++ is a computer programming language based on C.C++ became one of the most used programming languages in the world.</p>
+    </div>
+    </div>
+  );
+}
+export default Info;
